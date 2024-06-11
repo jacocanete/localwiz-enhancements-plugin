137831eb79fcf13d145a4e851b742b3d1454902a
jacocanete <jacogagarincanete@gmail.com>
2024-06-12 02:55:28 +0800

    chore: Bump version to 0.2.0 in lw-enhancements.php

--- lw-enhancements.php
+++ lw-enhancements.php
@@ -4,7 +4,7 @@
  * Description:       LocalWiz Enhancements plugin that introduces new blocks integrated from the DataForSEO API.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            Jaco Gagarin Canete
  * Author URI:		  jaco-portfolio.me
  * License:           GPL-2.0-or-later
