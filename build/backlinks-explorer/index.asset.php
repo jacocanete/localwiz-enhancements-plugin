4a50f2fe816773f8a346d8cfcb6caddbf3190710
jacocanete <jacogagarincanete@gmail.com>
2024-06-12 20:22:44 +0800

    update: block versions to 1.0.0 and plugin version to 1.0.0 due to UI revamp

--- build/backlinks-explorer/index.asset.php
+++ build/backlinks-explorer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '26d5b1d4a8333986b4f7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b4ef6c8b37ecd6e0b011');
--- build/citation-finder/index.asset.php
+++ build/citation-finder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b9129d53a4643578e560');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '29306bc8ab70d5d37837');
