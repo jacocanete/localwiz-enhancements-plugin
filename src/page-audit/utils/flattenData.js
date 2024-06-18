cb02f7f5b8ae210b4289752935b144d011b1f368
jacocanete <jacogagarincanete@gmail.com>
2024-06-18 13:40:51 +0800

    chore: Add Page Audit block for on-page SEO data

---
+++ src/page-audit/utils/flattenData.js
@@ -0,0 +1,27 @@
+function flattenObject(ob) {
+	const toReturn = {};
+
+	for (const [i, value] of Object.entries(ob)) {
+		if (typeof value == "object" && value !== null) {
+			const flatObject = flattenObject(value);
+			for (const [x, flatValue] of Object.entries(flatObject)) {
+				toReturn[`${i}.${x}`] = flatValue;
+			}
+		} else {
+			toReturn[i] = value;
+		}
+	}
+	return toReturn;
+}
+
+export function flattenData(data) {
+	const result = [];
+	for (const item of data) {
+		try {
+			result.push(flattenObject(item));
+		} catch (error) {
+			console.error(`Error flattening object: ${error}`);
+		}
+	}
+	return result;
+}
